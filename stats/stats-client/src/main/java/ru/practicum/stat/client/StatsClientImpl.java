b51ac1e31fb31753ca588d165c36d06bd233da35
нфÐyarichekp <yanormik@gmail.com>
2025-05-20 18:07:25 +0300

    Отправка на первое ревью основной сервис

--- stats/stats-client/src/main/java/ru/practicum/stat/client/StatsClientImpl.java
+++ stats/stats-client/src/main/java/ru/practicum/stat/client/StatsClientImpl.java
@@ -3,7 +3,6 @@ package ru.practicum.stat.client;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
